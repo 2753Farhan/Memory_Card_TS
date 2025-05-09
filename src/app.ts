adede10f89b096c10d9faa450906182083d080a5
Farhan Islam <shuvro.cadet@gmail.com>
2025-05-09 12:10:56 +0600

    Feature: Convert to Typescript from Javascript

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import { startGame } from './game.js';
+
+const gameControlButton = document.getElementById("start-button") as HTMLButtonElement | null;
+const restartButton = document.getElementById("restart-button") as HTMLButtonElement | null;
+const winMessage = document.getElementById("win-message") as HTMLDivElement | null;
+
+
+
+
+
+restartButton?.addEventListener("click", () => {
+  if(!restartButton || !winMessage) {
+    return;
+  }
+  startGame();
+  winMessage.style.display = "none";
+  restartButton.style.display = "none";
+});
+
+
+gameControlButton?.addEventListener("click", () => {
+  if (!gameControlButton) {
+    return;
+  }
+  startGame();
+  gameControlButton.textContent = "Restart Game";
+   
+});
